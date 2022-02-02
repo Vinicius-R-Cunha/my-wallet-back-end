e748da956aac7076707ca8522c1b6fb203ba08a0
Vinicius <vinicunha2008@gmail.com>
2022-02-02 12:41:54 -0300

    sign in post

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import joi from 'joi';
 import dotenv from 'dotenv';
 import { MongoClient } from 'mongodb';
 import bcrypt from 'bcrypt';
+import { v4 as uuid } from 'uuid';
 
 dotenv.config();
 
@@ -18,7 +19,7 @@ app.use(express.json());
 app.use(cors());
 
 app.post('/sign-up', async (req, res) => {
-    const loginSchema = joi.object({
+    const signUpSchema = joi.object({
         name: joi.string().required(),
         email: joi.string().lowercase().strict().required(),
         password: joi.string().required()
@@ -28,13 +29,13 @@ app.post('/sign-up', async (req, res) => {
         const user = req.body;
         const passwordHash = bcrypt.hashSync(user.password, 10);
 
-        const validation = loginSchema.validate(user, { abortEarly: false });
+        const validation = signUpSchema.validate(user, { abortEarly: false });
         if (validation.error) {
             return res.status(422).send(validation.error.details.map(obj => (obj.message)));
         }
 
         const userExists = await db.collection('users').findOne({ email: user.email });
-        if (userExists) {
+        if (!userExists) {
             return res.sendStatus(409);
         }
 
@@ -46,6 +47,41 @@ app.post('/sign-up', async (req, res) => {
     }
 });
 
+app.post('/sign-in', async (req, res) => {
+    const signInSchema = joi.object({
+        email: joi.string().lowercase().strict().required(),
+        password: joi.string().required()
+    });
+
+    try {
+        const user = req.body;
+
+        const validation = signInSchema.validate(user, { abortEarly: false });
+        if (validation.error) {
+            return res.status(422).send(validation.error.details.map(obj => (obj.message)));
+        }
+
+        const userExists = await db.collection('users').findOne({ email: user.email });
+        const passwordIsCorrect = bcrypt.compareSync(req.body.password, userExists.password);
+
+        if (userExists && passwordIsCorrect) {
+            const token = uuid();
+
+            await db.collection('sessions').insertOne(
+                {
+                    userId: userExists._id,
+                    token
+                }
+            );
+
+            res.status(200).send(token);
+        } else {
+            return res.sendStatus(409);
+        }
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
 
 
 app.listen(5000);
