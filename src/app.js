595ecb51be0d3b946f12ddc1605863b021f2a949
Vinicius <vinicunha2008@gmail.com>
2022-02-10 09:55:59 -0300

    chore: adjust code for deploy on heroku

--- src/app.js
+++ src/app.js
@@ -10,6 +10,6 @@ app.use(cors());
 app.use(authRouter);
 app.use(walletRouter);
 
-app.listen(5000, () => {
-    console.log('Server is listening on port 5000.');
+app.listen(process.env.PORT, () => {
+    console.log("Server running on port " + process.env.PORT);
 });
