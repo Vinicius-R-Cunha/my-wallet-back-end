f7c8cf7870ae12e21eb4186de3cdee3977c90c59
Vinicius <vinicunha2008@gmail.com>
2022-02-10 10:09:41 -0300

    chore: adjust code for deploy on heroku 2.0

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import express from 'express';
-//import cors from 'cors';
+import cors from 'cors';
 import authRouter from './routes/authRouter.js';
 import walletRouter from './routes/walletRouter.js';
 
 const app = express();
 app.use(express.json());
-//app.use(cors());
+app.use(cors());
 
 app.use(authRouter);
 app.use(walletRouter);
